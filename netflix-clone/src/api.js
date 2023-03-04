98c07d6c2a045e892aa449aeaeba912b4c0d9ee5
Otávio Faria <oaugustofaria@gmail.com>
2023-03-04 16:34:58 -0300

    Adicionando clone netflix

---
+++ netflix-clone/src/api.js
@@ -0,0 +1,51 @@
+const API_KEY = "a1557f54059b3decbf669756b3e753e5";
+
+const categories = [
+  {
+    name: "trending",
+    title: "Em alta",
+    path: `/trending/all/week?api_key=${API_KEY}&language=pt-BR`,
+    isLarge: true,
+  },
+  {
+    name: "netflixOriginals",
+    title: "Originais Netflix",
+    path: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+    isLarge: false,
+  },
+  {
+    name: "topRated",
+    title: "Populares",
+    path: `/movie/top_rated?api_key=${API_KEY}&language=pt-BR`,
+    isLarge: false,
+  },
+  {
+    name: "comedy",
+    title: "Comédias",
+    path: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+    isLarge: false,
+  },
+  {
+    name: "romances",
+    title: "Romances",
+    path: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+  },
+  {
+    name: "documentaries",
+    title: "Documentários",
+    path: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+    isLarge: false,
+  },
+];
+
+export const getMovies = async (path) => {
+  try {
+    let url = `https://api.themoviedb.org/3/${path}`;
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.log("error getMovies: ", error);
+  }
+};
+
+export default categories;
